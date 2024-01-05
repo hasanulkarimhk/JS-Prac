336fe3c6bc5e9ebece895c1fd8373a91564b4844
hasanulkarimhk <hasanulkarimhk@yahoo.com>
2024-01-05 11:57:51 +0600

    JavaScript

---
+++ Input.js
@@ -0,0 +1,6 @@
+let userInput = window.prompt("Write something: ");
+
+console.log("You entered: " + userInput);
+
+// I think JS can't take input in the editor but browser.
+// I will update this file if I get to knoe something otherwise.
