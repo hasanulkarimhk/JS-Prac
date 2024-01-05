c7d25be90386c0a048117bdb2ec0e009982ea282
hasanulkarimhk <hasanulkarimhk@yahoo.com>
2024-01-05 17:36:37 +0600

    Initial Commit

--- Unsaved.js
+++ Unsaved.js
@@ -1,5 +1,69 @@
-const oneMap = new Map();
+// const oneMap = new Map();
 
-oneMap.set("ai", 32);
-console.log(oneMap.get('ai'));
-// it prints 32 🙏
+// oneMap.set("ai", 32);
+// console.log(oneMap.get('ai'));
+// // it prints 32 🙏
+
+// const oneMap = new Map();
+
+// oneMap.set("ai", 32);
+// oneMap.set("bi", 3);
+// oneMap.set("ci", 98);
+// oneMap.set("di", 42);
+
+// console.log(oneMap);
+// //THis prints Map(4) { 'ai' => 32, 'bi' => 3, 'ci' => 98, 'di' => 42 }
+
+// oneMap.set("ai", 32);
+// oneMap.set("bi", 3);
+// oneMap.set("ci", 98);
+// oneMap.set("di", 42);
+
+// oneMap.set("ai", 82);
+
+// console.log(oneMap.get('ai'));
+
+// //This prints 82
+
+
+// oneMap.set("ai", 32);
+// oneMap.set("bi", 3);
+// oneMap.set("ci", 98);
+// oneMap.set("di", 42);
+
+// oneMap.set("Ai", 82);
+
+// console.log(oneMap.get('ai'));
+
+// // This prints 82
+
+
+// oneMap.set("ai", 32);
+// oneMap.set("bi", 3);
+// oneMap.set("ci", 98);
+// oneMap.set("di", 42);
+
+// console.log(oneMap.size);
+
+// // This prints 4
+
+// const oneMap = new Map();
+
+// oneMap.set("ai", 32);
+// oneMap.set("bi", 3);
+// oneMap.set("ci", 98);
+// oneMap.set("di", 42);
+
+// oneMap.delete('di');
+
+// console.log(oneMap);
+
+// // This prints Map(3) { 'ai' => 32, 'bi' => 3, 'ci' => 98 }
+
+const contacts = new Map();
+contacts.set("Jessie", { phone: "213-555-1234", address: "123 N 1st Ave" });
+
+console.log(contacts);
+
+
+
